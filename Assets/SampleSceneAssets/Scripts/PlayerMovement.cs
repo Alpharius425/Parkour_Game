7c7dc29b155c37b035237123d434df7cfe1c02cd
Mikey <mikeyp895@gmail.com>
2021-07-07 16:19:46 -0700

    Crouch functionality is set up. will likely need some changing of variable numbers but the code works

--- Assets/SampleSceneAssets/Scripts/PlayerMovement.cs
+++ Assets/SampleSceneAssets/Scripts/PlayerMovement.cs
@@ -13,11 +13,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] PlayerInput myInputs; // saves our input system and allows us to change controls for different controllers or settings
 
     [SerializeField] float playerDefaultSpeed;
-    public float curSpeed = 12f;
-    public float gravity = -10f;
-    public float jumpHeight = 2f;
+    public float curSpeed = 12f; // how fast we are currently going
+    public float gravity = -10f; // how hard we are affected by gravity
+    public float jumpHeight = 2f; // how much force we use to jump
 
-    Vector2 inputMovement;
+    Vector2 inputMovement; // saves input from movement keys
 
     Vector3 move; // controls our movement and speed
 
@@ -27,7 +27,7 @@ public class PlayerMovement : MonoBehaviour
     
 
     Vector3 velocity;
-    public bool isGrounded;
+    public bool isGrounded; // checks if we are on the ground or in the air
 
     [Header("Game Objects")]
     public Camera cameraComponent;
@@ -36,7 +36,19 @@ public class PlayerMovement : MonoBehaviour
     public float sprintSpeed = 15f; // speed multiplier
     public float cameraSprintFOV = 105; // FOV when sprinting
     float cameraDefaultFOV; // used to save our default FOV
-    public bool sprinting;
+    public bool sprinting; // checks if the player is sprinting
+
+    [Header("Player Crouch Options")]
+    public float defaultPlayerHeight; // height for our collider when not crouching
+    public float crouchedPlayerHeight; // height for our collider when crouched
+    public float defaultPlayerCenter; // height for our collider when not crouching
+    public float crouchedPlayerCenter; // height for our collider when crouched
+    [SerializeField] CapsuleCollider myCollider; // saves our collider so we can edit it
+    [SerializeField] float crouchSpeed;
+
+    public float defaultCamHeight; // height for our camera when not crouching
+    public float crouchedCamHeight; // height for our camera when crouched
+    bool crouched = false;
 
     //#if ENABLE_INPUT_SYSTEM
     //public InputAction movement;
@@ -155,6 +167,33 @@ public class PlayerMovement : MonoBehaviour
         {
             sprinting = false;
             ResetSpeed();
+        }
+    }
+
+    public void Crouch(InputAction.CallbackContext value)
+    {
+        if(!sprinting) // checks if we are sprinting or not
+        {
+            if (value.started) // if we are sprinting and holding down the crouch button we will lower the player's collider and camera as well as set our speed to the crouch speed
+            {
+                crouched = true;
+                myCollider.height = crouchedPlayerHeight;
+                myCollider.center = new Vector3(0f, crouchedPlayerCenter, 0f);
+                cameraComponent.transform.localPosition = new Vector3(0f, crouchedCamHeight, 0f);
+                curSpeed = crouchSpeed;
+            }
+            if (value.canceled) // when we let go of crouch we will reset the player collider and camera as well as reseting our speed
+            {
+                crouched = false;
+                myCollider.height = defaultPlayerHeight;
+                myCollider.center = new Vector3(0f, defaultPlayerCenter, 0f);
+                cameraComponent.transform.localPosition = new Vector3(0f, defaultCamHeight, 0f);
+                ResetSpeed();
+            }
+        }
+        else // if we are sprinting TODO Sliding
+        {
+
         }
     }
 
