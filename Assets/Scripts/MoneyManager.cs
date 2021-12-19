6959763eb51bac911fe2b3a105ff5a0ef54724dd
Edrik Padilla <edrikpadilla@gmail.com>
2021-12-19 00:48:46 -0800

    Added MoneyUI elements in scene.

--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoneyManager : MonoBehaviour
 {
     public static MoneyManager Instance;
+
+    [SerializeField] private TextMeshProUGUI money;
+    [SerializeField] private TextMeshProUGUI moneyCard;
+
+    [HideInInspector] public int Money;
 
     private void Awake()
     {
