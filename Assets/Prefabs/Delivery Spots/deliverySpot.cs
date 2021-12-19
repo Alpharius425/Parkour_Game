b42355c733165bc46fda7d53901e96ab11648a5c
Edrik Padilla <edrikpadilla@gmail.com>
2021-12-19 01:44:17 -0800

    Added the delivery spots in the scene. Added UI elements to show that the player got money from each package delivered.

--- Assets/Prefabs/Delivery Spots/deliverySpot.cs
+++ Assets/Prefabs/Delivery Spots/deliverySpot.cs
@@ -6,12 +6,15 @@ public class deliverySpot : MonoBehaviour
 {
     public GameObject[] deliverySpots;
     int deliverySpotCount = 0;
+    [SerializeField] private MoneyManager moneyManager;
+    [SerializeField] private int moneyReward;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Package")
         {
             Destroy(other.gameObject);
+            moneyManager.AddMoney(moneyReward);
 
             if (deliverySpotCount == deliverySpots.Length)
             {
--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -7,13 +7,32 @@ public class MoneyManager : MonoBehaviour
 {
     public static MoneyManager Instance;
 
-    [SerializeField] private TextMeshProUGUI money;
-    [SerializeField] private TextMeshProUGUI moneyCard;
+    // UI Elements; MoneyGroup
+    [SerializeField] private TextMeshProUGUI moneyText;
+    [SerializeField] private TextMeshProUGUI moneyCardText;
 
-    [HideInInspector] public int Money;
+    [SerializeField] private float moneyCardCountdown;
+
+    [HideInInspector] public int money;
 
     private void Awake()
     {
         Instance = this;
+        money = 0;
+    }
+
+    // Adds money to the total money, and displays it on the Money Card text for a brief time.
+    public void AddMoney(int value) {
+        money += value;
+        moneyText.SetText("$" + money);
+        moneyCardText.SetText("+ $" + value);
+        moneyCardText.gameObject.SetActive(true);
+        Invoke("DisableMoneyCard", moneyCardCountdown); // Calls DisableMoneyCard(), Interval seconds from now.
+        return;
+    }
+
+    private void DisableMoneyCard() {
+        moneyCardText.gameObject.SetActive(false);
+        return;
     }
 }
