7804dbbae9219d0aa382d19f494280afb578bc22
Edrik Padilla <s-edrik.padilla@lwtech.edu>
2022-01-20 19:46:50 -0800

    Additions to MusicAudioSource.cs: > Added a debug volume slider. > Added PlayRandomMusicClip() function. > When the music audio source stops playing (clip playing ends), it will call the PlayRandomMusicClip() function to play another random clip.

--- Assets/Scripts/MusicAudioSource.cs
+++ Assets/Scripts/MusicAudioSource.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class MusicAudioSource : MonoBehaviour
 {
+    [Header("Settings")]
     public AudioClip[] musicClips;
     [SerializeField] private bool musicIsOn;
 
+    [Header("Debug")]
+    [Space(5)]
+    [Range(0.0f,1.0f)]
+    [SerializeField] private float debugVolume;
+    private float volume;
+
     private void Awake() {
         if (musicIsOn) {
-            GetComponent<AudioSource>().clip = musicClips[Random.Range(0, musicClips.Length)];
-            GetComponent<AudioSource>().Play();
+            PlayRandomMusicClip();
         }
+    }
+
+    private void Update() {
+        if (musicIsOn) {
+            // Updates AudioSource volume only if there's been a change.
+            if (volume != debugVolume) {
+                volume = debugVolume;
+                GetComponent<AudioSource>().volume = volume;
+            }
+
+            // Plays another random music clip after the current clip playing is finished.
+            if (GetComponent<AudioSource>().isPlaying == false) {
+                PlayRandomMusicClip();
+            }
+        } 
+    }
+
+    public void PlayRandomMusicClip() {
+        GetComponent<AudioSource>().clip = musicClips[Random.Range(0, musicClips.Length)];
+        GetComponent<AudioSource>().Play();
     }
 }
