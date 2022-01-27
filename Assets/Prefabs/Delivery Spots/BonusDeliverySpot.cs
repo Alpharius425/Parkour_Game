fd10167ea5e79e61060daaf6def358a9d65c9db0
Edrik Padilla <edrikpadilla@gmail.com>
2022-01-26 16:40:17 -0800

    script changes

--- Assets/Prefabs/Delivery Spots/BonusDeliverySpot.cs
+++ Assets/Prefabs/Delivery Spots/BonusDeliverySpot.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class BonusDeliverySpot : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] private GameObject bonusPackageObject;
+
+    [Header("VFX")]
+    [Space(5)]
+    [SerializeField] private GameObject vfxObject;
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject == bonusPackageObject) {
             other.GetComponent<BonusPackage>().BonusPackageDelivered();
+            SpawnVFX();
             Destroy(gameObject);
         }
         else 
         return;
+    }
+
+    private void SpawnVFX() {
+        Instantiate(vfxObject, gameObject.transform.position, transform.rotation);
     }
 }
