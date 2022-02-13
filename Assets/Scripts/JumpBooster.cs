0bfbf2f398f9d42472cd3d29f5b7be724e566007
Edrik Padilla <edrikpadilla@gmail.com>
2022-02-12 16:34:25 -0800

    Re-worked JumpBooster.cs to have a static jump multiplier amount AND a static air speed, both of which can be changed if necessary only.

--- Assets/Scripts/JumpBooster.cs
+++ Assets/Scripts/JumpBooster.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class JumpBooster : MonoBehaviour
 {
+    [SerializeField] private string Notes;
+
+    [Space(5)]
     public float jumpMultiplier;
+    
+    [Space(5)]
     public float airSpeed;
+
+    [Space(5)]
+    public bool useAirSpeedBoost;
+    public float airSpeedBoost;
+
+    private void Start() {
+        if (useAirSpeedBoost) {
+            airSpeed = airSpeedBoost;
+        }
+    }
 }
