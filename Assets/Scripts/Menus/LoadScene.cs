87c2283cdffde3aeaf88469545c9e0158eab2bbc
Mikey <mikeyp895@gmail.com>
2022-02-25 20:38:53 -0800

    loading screen work but when full screen the loading waits an extra few seconds after the bar is full to load the scene and while windowed it takes an extra few seconds to start the loading bar

--- Assets/Scripts/Menus/LoadScene.cs
+++ Assets/Scripts/Menus/LoadScene.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
     public static LoadScene instance;
     [SerializeField] int level;
 
@@ -16,6 +21,28 @@ public class LoadScene : MonoBehaviour
     public void LoadByIndex()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene(level);
+        
+        loadingScreen.SetActive(true);
+        StartCoroutine(loadAsynchronously());
+    }
+
+    IEnumerator loadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+
+        while(operation.isDone == false)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            Debug.Log(progress);
+
+            //if (operation.isDone)
+            //{
+            //    loadingScreen.SetActive(false);
+            //}
+
+            yield return null;
+        }
     }
 }
