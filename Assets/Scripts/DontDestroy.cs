0107a76409a5d8a8b51c18bacafa44ea86fddccc
Mikey <mikeyp895@gmail.com>
2022-03-09 19:50:33 -0800

    made script to save objects tagged with DontDestroy when swapping to scene where there is an object with the script

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    GameObject[] savedObjects;
+
+    private void Awake()
+    {
+        savedObjects = GameObject.FindGameObjectsWithTag("DontDestroy");
+
+        for (int i = 0; i < savedObjects.Length; i++)
+        {
+            DontDestroyOnLoad(savedObjects[i]);
+        }
+    }
+}
