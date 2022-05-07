bb58488bf931031d8b7411083c676c154c73ef08
Mikey <mikeyp895@gmail.com>
2022-05-06 22:18:21 -0700

    Completely redid the delivery spot system and added triggers to determine where the delivery spot should be if we progress to far as well as a trigger to activate other game objects

--- Assets/Scripts/Triggers/ActivateItem.cs
+++ Assets/Scripts/Triggers/ActivateItem.cs
@@ -7,12 +7,18 @@ public class ActivateItem : MonoBehaviour
     // script saves a list of gameobjects and activates them with a trigger collider
 
     GameObject[] activatedObjects;
+    [SerializeField] bool triggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        foreach (GameObject item in activatedObjects)
+        if(!triggered)
         {
-            item.SetActive(true);
+            foreach (GameObject item in activatedObjects)
+            {
+                item.SetActive(true);
+            }
+
+            triggered = true;
         }
     }
 }
