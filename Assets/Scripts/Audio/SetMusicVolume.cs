c87af3e42d0082cf0cdc3fc2465ec5d39d03a554
Hannah Geer <44559340+HannahWritesCode@users.noreply.github.com>
2022-05-13 00:41:56 -0700

    Added persistence to volume slider levels

--- Assets/Scripts/Audio/SetMusicVolume.cs
+++ Assets/Scripts/Audio/SetMusicVolume.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetMusicVolume : MonoBehaviour
 {
@@ -9,9 +10,16 @@ public class SetMusicVolume : MonoBehaviour
     // https://johnleonardfrench.com/the-right-way-to-make-a-volume-slider-in-unity-using-logarithmic-conversion
 
     public AudioMixer mixer;
-
+    public Slider slider;
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolumePref", 1);
+    }
     public void SetLevel(float sliderValue)
     {
+        //float sliderValue = slider.value;
         mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolumePref", sliderValue);
     }
+
 }
