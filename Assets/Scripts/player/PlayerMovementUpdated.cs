28f660a7b83723e74a97003a71bc52265f252e8a
Mikey <mikeyp895@gmail.com>
2022-05-20 19:56:17 -0700

    Vault code no longer forces player to look at the position they are vaulting to

--- Assets/Scripts/player/PlayerMovementUpdated.cs
+++ Assets/Scripts/player/PlayerMovementUpdated.cs
@@ -575,7 +575,7 @@ public class PlayerMovementUpdated : MonoBehaviour
         //Debug.Log("Should vault");
         startTime = Time.time;
         oldLocation = gameObject.transform.position;
-        gameObject.transform.LookAt(newLocation); // makes our player look at the endpoint
+        //gameObject.transform.LookAt(newLocation); // makes our player look at the endpoint
         journeyDistance = Vector3.Distance(oldLocation, newLocation);
 
         center = (oldLocation + newLocation) * 0.5F;
@@ -592,11 +592,6 @@ public class PlayerMovementUpdated : MonoBehaviour
         //myLegAnimator.SetBool("Vaulting", true);
         //myArmAnimator.SetTrigger("Vaulting");
         myArmAnimator.SetTrigger("Vaulting");
-    }
-
-    void VaultMovement(Vector3 endPoint)
-    {
-
     }
 
     private void CancelZandXRotation() {
