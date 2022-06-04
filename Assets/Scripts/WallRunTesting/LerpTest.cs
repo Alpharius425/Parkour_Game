1948dbb6ccd17d034f056650d3bc0aa3dc1ad77a
Paul Gibbons <31017045+QuestGiver@users.noreply.github.com>
2022-06-03 20:54:32 -0700

    add wall running is usable form

--- Assets/Scripts/WallRunTesting/LerpTest.cs
+++ Assets/Scripts/WallRunTesting/LerpTest.cs
@@ -11,6 +11,7 @@ public class LerpTest : MonoBehaviour
     [SerializeField] bool allowWallRun;
     [SerializeField] float Speed = 10;
     [SerializeField] float savedGravity;
+    [SerializeField] BoxCollider boxCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -21,23 +22,39 @@ public class LerpTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            allowWallRun = !allowWallRun;
-            MovementControl.gravity = 0;
-        }
+            //allowWallRun = !allowWallRun;
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            MovementControl.gravity = savedGravity;
+            if (allowWallRun)
+            {
+                MovementControl.gravity = 0;
+                MovementControl.ResetVelocity();
+            }
+            else
+            {
+                MovementControl.gravity = savedGravity;
+            }
         }
 
         if (allowWallRun)
         {
             Controller.myMovement.MoveVelocity((Goal_B.position - Goal_A.position).normalized * 15);
         }
+    }
 
+    void OnTriggerEnter(Collider collider)
+    {
+        allowWallRun = true;
+        MovementControl.gravity = 0;
+        MovementControl.ResetVelocity();
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        allowWallRun = false;
+        MovementControl.gravity = savedGravity;
+        Controller.myMovement.MoveVelocity((Controller.transform.forward));
     }
 }
+
