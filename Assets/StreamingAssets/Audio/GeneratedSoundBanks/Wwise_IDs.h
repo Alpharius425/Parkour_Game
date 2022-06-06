1365c4086cb7790c6fdfc5b06b7fc990cdf1f1f6
Cameron <Camtaelor@gmail.com>
2022-06-05 20:48:49 -0700

    Committed 1312 previously unstaged audio files.

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,111 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PICKUP_BOX = 1589366019U;
+        static const AkUniqueID PLAY_WIND = 1020223172U;
+        static const AkUniqueID PLAYER_MOVEMENT = 541470702U;
+        static const AkUniqueID STOP_PLAYER_MOVEMENT = 1386092461U;
+        static const AkUniqueID STOP_WIND = 3173136834U;
+        static const AkUniqueID THROW_BOX = 2578410269U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace BOXCONTENT
+        {
+            static const AkUniqueID GROUP = 3656819559U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace BOXCONTENT
+
+        namespace GROUNDTYPE
+        {
+            static const AkUniqueID GROUP = 2835351336U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID AIR = 1050421051U;
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GLASS = 2449969375U;
+            } // namespace SWITCH
+        } // namespace GROUNDTYPE
+
+        namespace PLAYER_SPEED
+        {
+            static const AkUniqueID GROUP = 1062779386U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID RUN = 712161704U;
+                static const AkUniqueID STOP = 788884573U;
+                static const AkUniqueID WALK = 2108779966U;
+            } // namespace SWITCH
+        } // namespace PLAYER_SPEED
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIENCE_VOLUME = 2901143258U;
+        static const AkUniqueID MASTER_VOLUME = 4179668880U;
+        static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PLAYER_FLOORDIST = 2941405957U;
+        static const AkUniqueID PLAYER_SPEED = 1062779386U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SFX_VOLUME = 1564184899U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SOUNDFX = 2810670744U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID REVERBSFX = 3964740718U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
