1a211cc9a823ee41a96008dd1c70c6abcd496bc5
Hannah Geer <44559340+HannahWritesCode@users.noreply.github.com>
2022-07-11 18:08:24 -0700

    Made settings menu accessible from main menu

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [Header("UI Panels")]
     public GameObject SettingsMenuUI;
     public GameObject KeybindsMenuUI;
-    public bool settingsMenuActive = false;
-    public bool keybindsMenuActive = false;
+
+    [Header("Button text displays")]
+    public TextMeshProUGUI crouchButtonText;
+    public TextMeshProUGUI sprintButtonText;
+
+    [Header("Toggle settings")]
+    public bool crouchToggledSetting;
+    public bool sprintToggledSetting;
+
+    private bool settingsMenuActive = false;
+    private bool keybindsMenuActive = false;
+
     public static SettingsMenu instance;
 
     private void Awake()
@@ -19,14 +32,13 @@ public class SettingsMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("settings menu start");
         SettingsMenuUI.SetActive(false);
         KeybindsMenuUI.SetActive(false);
+        GetCrouchAndSprintPreferences();
     }
 
-    public void SettingsMenuToggle()
+    public void ToggleSettingsMenu()
     {
-        //Debug.Log("settings menu toggled");
         if (settingsMenuActive == false)
         {
             settingsMenuActive = true;
@@ -44,9 +56,8 @@ public class SettingsMenu : MonoBehaviour
             SettingsMenuUI.SetActive(false);
         }
     }
-    public void KeybindsMenuToggle()
+    public void ToggleKeybindsMenu()
     {
-        //Debug.Log("keybinds menu toggled");
         if (keybindsMenuActive == false)
         {
             keybindsMenuActive = true;
@@ -61,4 +72,67 @@ public class SettingsMenu : MonoBehaviour
             KeybindsMenuUI.SetActive(false);
         }
     }
+
+    public void GetCrouchAndSprintPreferences()
+    {
+        // get player preferences or set both to toggle detection by default 
+        int crouchToggledInt = PlayerPrefs.GetInt("CrouchTogglePref", 1); 
+        int sprintToggledInt = PlayerPrefs.GetInt("SprintTogglePref", 1);
+
+        if (crouchToggledInt == 0)
+        {
+            crouchButtonText.text = "Hold";
+            crouchToggledSetting = false;
+        }
+        else if (crouchToggledInt == 1)
+        {
+            crouchButtonText.text = "Tap";
+            crouchToggledSetting = true;
+        }
+
+        if (sprintToggledInt == 0)
+        {
+            sprintButtonText.text = "Hold";
+            sprintToggledSetting = false;
+        }
+        else if (sprintToggledInt == 1)
+        {
+            sprintButtonText.text = "Tap";
+            sprintToggledSetting = true;
+        }
+
+    }
+
+    public void ToggleCrouchSetting()
+    {
+        if (crouchToggledSetting == true)
+        {
+            PlayerPrefs.SetInt("CrouchTogglePref", 0);
+            crouchButtonText.text = "Hold";
+            crouchToggledSetting = false;
+        }
+        else if (crouchToggledSetting == false)
+        {
+            PlayerPrefs.SetInt("CrouchTogglePref", 1);
+            crouchButtonText.text = "Tap";
+            crouchToggledSetting = true;
+        }
+    }
+    public void ToggleSprintSetting()
+    {
+        if (sprintToggledSetting == true)
+        {
+            PlayerPrefs.SetInt("SprintTogglePref", 0);
+            sprintButtonText.text = "Hold";
+            sprintToggledSetting = false;
+
+        }
+        else if (sprintToggledSetting == false)
+        {
+            PlayerPrefs.SetInt("SprintTogglePref", 1);
+            sprintButtonText.text = "Tap";
+            sprintToggledSetting = true;
+        }
+    }
+
 }
